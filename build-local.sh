e1748bcdecb7ba927537b77ed14eeb8bfd69433a
Paulo Monteiro <paulo.from.portugal@gmail.com>
2017-05-06 15:38:55 +0100

    Adding docker command to run the container

--- build-local.sh
+++ build-local.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-mvn -DskipTests clean package && docker build -t pauloportugal/dropwizard-guice-jpa:local .
+mvn -DskipTests clean package && \
+docker build -t pauloportugal/dropwizard-guice-jpa:local . && \
+docker run -ti --rm --name guice-jpa pauloportugal/dropwizard-guice-jpa:local
